1f9934555ea3d65a133620257e7a3949d8127c19
Gift-Lumumba <giftlumumba2@gmail.com>
2018-09-17 17:28:29 +0300

    Added changes

--- manage.py
+++ manage.py
@@ -1,13 +1,30 @@
-
-from flask_script import Manager,Server
 from app import create_app,db
+from flask_script import Manager,Shell,Server
+from  flask_migrate import Migrate, MigrateCommand
+from app.models import User,Pitch,Comment,Upvote,Downvote
 
-app = create_app('default')
+# Creating app instance
+app = create_app('production')
+app = create_app('test')
 
+
+
+migrate = Migrate(app,db)
 manager = Manager(app)
+manager.add_command('db',MigrateCommand)
+manager.add_command('server',Server)
 
-manager.add_command('server', Server)
+@manager.command
+def test():
+    """Run the unit tests."""
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+
+@manager.shell
+def make_shell_context():
+    return dict(app = app,db = db,User = User,Pitch = Pitch,Comment = Comment,Upvote = Upvote,Downvote = Downvote)
 
 if __name__ == '__main__':
+    # app.secret_key = 'gL0711'
     manager.run()
-
