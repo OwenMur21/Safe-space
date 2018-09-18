f1cd81d8cc2442c14235f33f1a99b4e56893b76e
Owenmur21 <owenmuriithi@gmail.com>
2018-09-18 09:57:48 +0300

    Creates views for each category

--- app/main/views.py
+++ app/main/views.py
@@ -6,10 +6,58 @@ from flask import render_template
 @main.route('/')
 def home():
     """
-    Functiom  that renders the home page
+    Function that renders the home page
     """
     title="Welcome | Safe Space"
 
     return render_template('home.html', title=title)
+
+
+@main.route('/index')
+def index():
+    """
+    Function that returns the index which has all the categories
+    """
+
+    return render_template('index.html')
+
+
+@main.route('/identity')
+def identity():
+    """
+    Function that chooses the identity crisis category
+    """
+
+    return render_template('crisis.html')
+
+
+@main.route('/fam')
+def fam():
+    """
+    Function that chooses the family issues category
+    """
+
+    return render_template('fam.html')
+
+
+@main.route('/depression')
+def depression():
+    """
+    Function that chooses the depression category
+    """
+
+    return render_template('fam.html')
+
+
+@main.route('/health')
+def health():
+    """
+    Function that chooses the family issues category
+    """
+
+    return render_template('health.html')
+
+
+
 
     
