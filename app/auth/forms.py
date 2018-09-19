7614f86d3095addcd61c0e48d3340108682298d6
Gift-Lumumba <giftlumumba2@gmail.com>
2018-09-19 12:06:30 +0300

    Fixes

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,13 +1,13 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField,PasswordField,SubmitField, BooleanField
 from wtforms.validators import Required,Email,EqualTo
+from ..models import User
 from wtforms import ValidationError
 
 
 class RegistrationForm(FlaskForm):
 
     email = StringField('Your Email Address',validators=[Required(),Email()])
-    username = StringField('Enter your username',validators = [Required()])
     password = PasswordField('Password',validators = [Required(),
     EqualTo('password_confirm',message = 'Passwords must match')])
     password_confirm = PasswordField('Confirm Passwords',validators = [Required()])
--- app/auth/views.py
+++ app/auth/views.py
@@ -4,6 +4,7 @@ from .forms import RegistrationForm, LoginForm
 from .. import db
 from flask_login import login_user, login_required, logout_user
 from ..email import mail_message
+from ..models import User
 
 #....
 @auth.route('/login',methods=['GET','POST'])
@@ -27,7 +28,7 @@ def login():
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
-        user = User(email = form.email.data, username = form.username.data,password = form.password.data)
+        user = User(email = form.email.data,password = form.password.data)
         db.session.add(user)
         db.session.commit()
 
