76ab108dbacb2d4056813c7f6665923e1f0ebea1
Owenmur21 <owenmuriithi@gmail.com>
2018-09-20 15:49:31 +0300

    Adds email

--- app/email.py
+++ app/email.py
@@ -4,7 +4,7 @@ from . import mail
 
 
 def mail_message(subject,template,to,**kwargs):
-    sender_email='glumumba0@gmail.com'
+    sender_email='mathumuriithi@gmail.com'
     email = Message(subject, sender=sender_email, recipients=[to])
     email.body= render_template(template + ".txt",**kwargs)
     email.html = render_template(template + ".html",**kwargs)
